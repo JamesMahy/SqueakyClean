9dd0b64af2fcf6ee0b830a2c6b174ab7a0fea669
james.mahy <james.mahy@cevo.co.uk>
2018-09-26 15:16:14 +0100

    Initial Screen and Input absorbtion mechanism

--- Squeaky Clean/MainWindow.xaml.cs
+++ Squeaky Clean/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Squeaky_Clean
 {
@@ -20,9 +9,56 @@ namespace Squeaky_Clean
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Topmost = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_LostFocus(object sender, RoutedEventArgs e)
+        {
+            this.Focus();
+        }
+
+        public static void BlockInput(TimeSpan span)
+        {
+            try
+            {
+                NativeMethods.BlockInput(true);
+                Thread.Sleep(span);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                NativeMethods.BlockInput(false);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            BlockInput(new TimeSpan(0, 1, 0));
         }
     }
+
+    public partial class NativeMethods
+    {
+
+        /// Return Type: BOOL->int
+        ///fBlockIt: BOOL->int
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "BlockInput")]
+        [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool BlockInput([System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)] bool fBlockIt);
+
+    }
+
+   
 }
